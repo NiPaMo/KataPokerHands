46a9bf905bc7645711156c54bb7bcda073276389
Nick Moyer <moyer.n.p@gmail.com>
2018-05-21 18:08:30 -0400

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);
+		
+		System.out.println("Enter 5 card values for each player seperated by 1 space");
+		
+		System.out.print("Black: ");
+		String black = reader.nextLine();
+		
+		System.out.print("White: ");
+		String white = reader.nextLine();
+		
+		reader.close();
+		
+	}
+
+}
